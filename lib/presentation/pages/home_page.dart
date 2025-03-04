5cdc39be34e5f3b1a2a09e9fc8ddc2c05dc8788e
gmulonga <gadsonmulonga@gmail.com>
2025-03-04 12:48:24 +0300

    feat: add success dialog when deleteing todo

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo/data/models/todo_model.dart';
 import 'package:todo/logic/todo_bloc/todo_state.dart';
+import 'package:todo/presentation/widgets/confirmational_modal.dart';
 import 'package:todo/presentation/widgets/todo_tile.dart';
 import 'package:todo/core/constants.dart';
 import 'package:todo/presentation/widgets/searchbar.dart';
@@ -9,6 +10,7 @@ import 'package:todo/presentation/pages/add_todo_page.dart';
 import 'package:animations/animations.dart';
 import 'package:todo/logic/todo_bloc/todo_bloc.dart';
 import 'package:todo/logic/todo_bloc/todo_event.dart';
+import 'package:quickalert/quickalert.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -70,105 +72,128 @@ class _HomePageState extends State<HomePage> {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Container(
-                color: kNavyBlue,
-                height: screenHeight * 0.2,
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "TODO",
-                      style: TextStyle(
-                        color: kWhite,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
+    return BlocListener<TodoBloc, TodoState>(
+      listener: (context, state) {
+        if (state is DeleteTodoSuccess) {
+          confirmationModal(
+            context: context,
+            onConfirm: () {},
+            title: "Success",
+            message: "Todo Added successfully",
+            confirmButtonLabel: "Okay",
+            type: QuickAlertType.success,
+          );
+        } else if (state is DeleteTodoFailure) {
+          confirmationModal(
+            context: context,
+            onConfirm: () {},
+            title: "Error",
+            message: state.message,
+            confirmButtonLabel: "Okay",
+            type: QuickAlertType.success,
+          );
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  color: kNavyBlue,
+                  height: screenHeight * 0.2,
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "TODO",
+                        style: TextStyle(
+                          color: kWhite,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
                       ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.note_add, color: kWhite),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                left: screenWidth * 0.05,
-                right: screenWidth * 0.05,
-                bottom: -25,
-                child: Container(
-                  width: screenWidth * 0.9,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        blurRadius: 5,
-                        spreadRadius: 2,
-                        offset: const Offset(0, 2),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.note_add, color: kWhite),
                       ),
                     ],
                   ),
-                  child: Searchbar(controller: _textController),
                 ),
-              ),
-            ],
-          ),
-          Expanded(
-            child: BlocBuilder<TodoBloc, TodoState>(
-              builder: (context, state) {
-                if (state is TodoLoading) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (state is TodoLoaded) {
-                  final todos = _textController.text.isEmpty
-                      ? state.todos
-                      : _filteredTodos;
-                  return ListView.builder(
-                    padding: const EdgeInsets.symmetric(vertical: 30),
-                    itemCount: todos.length,
-                    itemBuilder: (context, index) {
-                      final todo = todos[index];
-                      return TodoTile(
-                        todo: todo,
-                        onCheckboxChanged: (value) =>
-                            toggleCheckbox(todos[index], value),
-                        onEdit: () => editTodo(todos[index]),
-                        onDelete: () => deleteTodo(todo),
-                      );
-                    },
-                  );
-                } else {
-                  return const Center(child: Text("No todos found"));
-                }
-              },
+                Positioned(
+                  left: screenWidth * 0.05,
+                  right: screenWidth * 0.05,
+                  bottom: -25,
+                  child: Container(
+                    width: screenWidth * 0.9,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 5,
+                          spreadRadius: 2,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Searchbar(controller: _textController),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: OpenContainer(
-        transitionDuration: const Duration(milliseconds: 500),
-        closedShape: const CircleBorder(),
-        closedElevation: 6,
-        closedColor: kNavyBlue,
-        openColor: Colors.white,
-        closedBuilder: (context, openContainer) {
-          return FloatingActionButton(
-            backgroundColor: kNavyBlue,
-            shape: const CircleBorder(),
-            child: const Icon(Icons.add, color: kWhite),
-            onPressed: openContainer,
-          );
-        },
-        openBuilder: (context, closeContainer) {
-          return const AddTodoPage();
-        },
+            Expanded(
+              child: BlocBuilder<TodoBloc, TodoState>(
+                builder: (context, state) {
+                  if (state is TodoLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state is TodoLoaded) {
+                    final todos = _textController.text.isEmpty
+                        ? state.todos
+                        : _filteredTodos;
+                    return ListView.builder(
+                      padding: const EdgeInsets.symmetric(vertical: 30),
+                      itemCount: todos.length,
+                      itemBuilder: (context, index) {
+                        final todo = todos[index];
+                        return TodoTile(
+                          todo: todo,
+                          onCheckboxChanged: (value) =>
+                              toggleCheckbox(todos[index], value),
+                          onEdit: () => editTodo(todos[index]),
+                          onDelete: () => deleteTodo(todo),
+                        );
+                      },
+                    );
+                  } else {
+                    return const Center(child: Text("No todos found"));
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: OpenContainer(
+          transitionDuration: const Duration(milliseconds: 500),
+          closedShape: const CircleBorder(),
+          closedElevation: 6,
+          closedColor: kNavyBlue,
+          openColor: Colors.white,
+          closedBuilder: (context, openContainer) {
+            return FloatingActionButton(
+              backgroundColor: kNavyBlue,
+              shape: const CircleBorder(),
+              child: const Icon(Icons.add, color: kWhite),
+              onPressed: openContainer,
+            );
+          },
+          openBuilder: (context, closeContainer) {
+            return const AddTodoPage();
+          },
+        ),
       ),
     );
   }
